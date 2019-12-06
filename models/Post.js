e8601742416d8703c7813d29fdc34277417b4146
KaoriYoshimura <infmalbyevo@gmail.com>
2019-12-06 15:40:26 +0100

    set up basic backend

---
+++ models/Post.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const postSchema = new Schema({
+    title: {
+        required: true,
+        type: String
+    },
+    image: {
+        required: true,
+        type: String
+    },
+    description: {
+        required: true,
+        type: String
+    },
+    // category: {
+    //     // required: true,
+    //     type: String
+    // },
+    // user: {
+    //     type: Schema.Types.ObjectId,
+    //     ref: 'users'
+    // },
+    // data: {
+    //     type: Date,
+    //     default: Date.now
+    // }
+});
+
+const Post = mongoose.model('users', postSchema)
+
+module.exports = Post
