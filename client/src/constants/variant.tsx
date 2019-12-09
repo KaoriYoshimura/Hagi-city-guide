297d5c2f6bf5c8283f2fe930bec682b905343947
KaoriYoshimura <infmalbyevo@gmail.com>
2019-12-09 19:40:59 +0100

    added variant constants

---
+++ client/src/constants/variant.tsx
@@ -0,0 +1,10 @@
+const VARIANTS = {
+    ACCESS: 'access',
+    GUIDE: 'guide',
+    WORLDHERITAGE: 'worldheritage',
+    FOOD: 'food',
+    REGINAL: 'regional',
+    INFO: 'info',
+};
+
+export default VARIANTS;
