6d383ef3c170063dfc383e0ec714cba5d0656636
KaoriYoshimura <infmalbyevo@gmail.com>
2019-12-11 18:42:52 +0100

    added postList in admin page

---
+++ client/src/components/posts/postListAdmin/index.tsx
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { faTrash, faEdit } from '@fortawesome/free-solid-svg-icons';
+import { fetchPosts } from '../../../actions/post/postActions';
+import { IPosts } from '../../../interfaces/interfaces';
+import classNames from 'classnames';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './postListAdmin.scss';
+
+interface IHomePostsProps {
+    props: any;
+    fetchPosts(): any;
+}
+
+class PostListAdmin extends Component<IHomePostsProps> {
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
+    render() {
+        const { posts } = this.props.props;
+        console.log(posts, posts.length);
+        return (
+            <div className={classNames('container', 'homePosts')}>
+                {/* {posts.map((post: IPosts) => (
+                    <article key={post.id}>
+                        <Link to="#">
+                            <div className="imageWrapper">
+                                <figure>
+                                    <img src={`/uploads/${post.image}`} alt={post.title} />
+                                </figure>
+                            </div>
+                            <div className="textWrapper">
+                                <div>{post.category}</div>
+                                <div>{post.title}</div>
+                            </div>
+                        </Link>
+                    </article>
+                ))} */}
+
+                <div className="post-list-admin-container">
+                    {/* Show table if there is post */}
+                    {posts.length > 0 ? (
+                        <div className="table-wrapper">
+                            <table>
+                                <thead>
+                                    <tr>
+                                        {/* Table header */}
+                                        <th>ID</th>
+                                        <th>Datum</th>
+                                        <th>Title</th>
+                                        <th>category</th>
+                                        <th>Edit</th>
+                                        <th>Remove</th>
+                                    </tr>
+                                </thead>
+                                {/* Table body */}
+                                <tbody>
+                                    {posts.map((post: IPosts) => (
+                                        <tr key={post._id}>
+                                            <td>{post._id}</td>
+                                            {/* <td>{post.date}</td> */}
+                                            <td>date</td>
+                                            <td>{post.title}</td>
+                                            <td>{post.category}</td>
+                                            <td className="list-center">
+                                                <FontAwesomeIcon
+                                                    icon={faEdit}
+                                                    className="admin-icon"
+                                                // onClick={() => this.decrement(bookings.booking_id)}
+                                                />
+                                            </td>
+                                            <td className="list-center">
+                                                <FontAwesomeIcon
+                                                    icon={faTrash}
+                                                    className="admin-icon"
+                                                // onClick={() => this.increment(bookings.booking_id)}
+                                                />
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    ) : (
+                            // Show message instead of table if there is no post
+                            <p>
+                                There is no post or database connection problem.
+              </p>
+                        )}
+                </div>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state: any) => ({
+    props: state.posts
+});
+
+// {fetchPosts: fetchPost} shorthand
+export default connect(mapStateToProps, { fetchPosts })(PostListAdmin);
--- client/src/pages/admin/index.tsx
+++ client/src/pages/admin/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PostForm from '../../components/posts/postForm';
+import PostListAdmin from '../../components/posts/postListAdmin';
 import classNames from 'classnames';
 import './admin.scss';
 
@@ -8,6 +9,8 @@ const Admin = () => (
         <main className={classNames('container', 'admin-container')}>
             <h2>Creat a new post</h2>
             <PostForm />
+            <h2>Post list</h2>
+            <PostListAdmin />
         </main>
     </>
 );
