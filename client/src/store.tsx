071d189f1ba1fc6b3a47e4bdbd5bb892746eb84b
KaoriYoshimura <infmalbyevo@gmail.com>
2019-12-28 08:06:46 +0100

    configured redux dev tool

--- client/src/store.tsx
+++ client/src/store.tsx
@@ -3,14 +3,13 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
 
 const initialState = {};
-const middleware = [thunk];
+
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
     rootReducer,
     initialState,
-    // Can use instead?
-    // applyMiddleware(thunk);
-    compose(applyMiddleware(...middleware))
+    composeEnhancers(applyMiddleware(thunk))
 );
 
 export default store;
